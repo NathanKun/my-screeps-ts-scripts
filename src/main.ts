74eed7d53e2737af23db2b7966d11c3a0940a7b5
NathanKun <nathanhejunyang@gmail.com>
2019-07-21 22:16:55 +0200

    start power bank condition update

--- src/main.ts
+++ src/main.ts
@@ -207,7 +207,7 @@ export const loop = ErrorMapper.wrapLoop(() => {
 
           // found, start power bank action
           if ((bank.length || (Memory.powerbank && Memory.powerbank.finished === false)) &&
-            bank[0].ticksToDecay > 4500) {
+            bank[0].ticksToDecay > 4500 && bank[0].power >= 2500) {
             Memory.powerbank = {
               bankId: bank[0].id,
               start: new RoomPosition(14, 1, 'W9S7'),
