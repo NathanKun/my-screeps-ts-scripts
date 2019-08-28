02268d65c409d96e65396ae0a1b8def35e7c246c
NathanKun <nathanhejunyang@gmail.com>
2019-08-28 20:12:46 +0200

    Add MarketUtil for selling energy

---
+++ src/utils/MarketUtil.ts
@@ -0,0 +1,19 @@
+export class MarketUtil {
+  public static doMarket(rooms: RoomConfig[]): void {
+    const terminals = (rooms
+      .map(r => r.room.terminal)
+      .filter(t => t && t.store.energy > 10000) as StructureTerminal[])
+      .sort((t1, t2) => t2.store.energy - t1.store.energy);
+
+    const sellOrders = Game.market.getAllOrders(
+      (order) =>
+        order.type === ORDER_BUY && order.resourceType === RESOURCE_ENERGY && order.price > 0.004
+    );
+
+    for (const order of sellOrders) {
+      const amount = order.amount > terminals[0].store.energy ? terminals[0].store.energy : order.amount;
+      Game.market.deal(order.id, amount, terminals[0].room.name)
+      console.log('Deal ' + amount + ' of energy with price ' + order.price);
+    }
+  }
+}
